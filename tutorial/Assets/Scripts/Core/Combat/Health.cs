594e9c31a6e8dc99aca744fdd9910980617e5ab0
tiag0cabral <tiago.cabral@eb.mil.br>
2025-06-09 12:23:59 -0300

    Implementando a barra de vida

---
+++ tutorial/Assets/Scripts/Core/Combat/Health.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Health : NetworkBehaviour
+{
+    [field: SerializeField] public int MaxHealth { get; private set; } = 100;
+    public NetworkVariable<int> CurrentHealth = new NetworkVariable<int>();
+    private bool isDead;
+    public Action<Health> OnDie;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) { return; }
+        CurrentHealth.Value = MaxHealth;
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        ModifyHealth(-damageValue);
+    }
+
+    public void RestoreHealth(int healthValue)
+    {
+        ModifyHealth(healthValue);
+    }
+
+    private void ModifyHealth(int value)
+    {
+        if (isDead) { return; }
+
+        int newHealth = CurrentHealth.Value + value;
+        CurrentHealth.Value = Mathf.Clamp(newHealth, 0, MaxHealth);
+
+        if (CurrentHealth.Value == 0)
+        {
+            OnDie?.Invoke(this);
+            isDead = true;
+        }
+    }
+}
